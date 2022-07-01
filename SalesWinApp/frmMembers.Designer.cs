d8ca49d4ed08cbd84ac620cb60f94c3028248da7
thanhnhan1402 <nhanttse151229@fpt.edu.vn>
2022-07-01 16:09:47 +0700

    Design Members form

--- SalesWinApp/frmMembers.Designer.cs
+++ SalesWinApp/frmMembers.Designer.cs
@@ -28,12 +28,217 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbMemberId = new System.Windows.Forms.Label();
+            this.txtMemberId = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lbEmail = new System.Windows.Forms.Label();
+            this.txtCompanyName = new System.Windows.Forms.TextBox();
+            this.lbCompanyName = new System.Windows.Forms.Label();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.lbCity = new System.Windows.Forms.Label();
+            this.txtCountry = new System.Windows.Forms.TextBox();
+            this.lbCountry = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lbPassword = new System.Windows.Forms.Label();
+            this.dgvMemberList = new System.Windows.Forms.DataGridView();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMemberList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbMemberId
+            // 
+            this.lbMemberId.AutoSize = true;
+            this.lbMemberId.Location = new System.Drawing.Point(31, 29);
+            this.lbMemberId.Name = "lbMemberId";
+            this.lbMemberId.Size = new System.Drawing.Size(66, 15);
+            this.lbMemberId.TabIndex = 0;
+            this.lbMemberId.Text = "Member ID";
+            // 
+            // txtMemberId
+            // 
+            this.txtMemberId.Location = new System.Drawing.Point(131, 29);
+            this.txtMemberId.Name = "txtMemberId";
+            this.txtMemberId.Size = new System.Drawing.Size(173, 23);
+            this.txtMemberId.TabIndex = 1;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(131, 73);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(173, 23);
+            this.txtEmail.TabIndex = 3;
+            // 
+            // lbEmail
+            // 
+            this.lbEmail.AutoSize = true;
+            this.lbEmail.Location = new System.Drawing.Point(31, 76);
+            this.lbEmail.Name = "lbEmail";
+            this.lbEmail.Size = new System.Drawing.Size(36, 15);
+            this.lbEmail.TabIndex = 2;
+            this.lbEmail.Text = "Email";
+            // 
+            // txtCompanyName
+            // 
+            this.txtCompanyName.Location = new System.Drawing.Point(131, 117);
+            this.txtCompanyName.Name = "txtCompanyName";
+            this.txtCompanyName.Size = new System.Drawing.Size(173, 23);
+            this.txtCompanyName.TabIndex = 5;
+            // 
+            // lbCompanyName
+            // 
+            this.lbCompanyName.AutoSize = true;
+            this.lbCompanyName.Location = new System.Drawing.Point(31, 120);
+            this.lbCompanyName.Name = "lbCompanyName";
+            this.lbCompanyName.Size = new System.Drawing.Size(94, 15);
+            this.lbCompanyName.TabIndex = 4;
+            this.lbCompanyName.Text = "Company Name";
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(454, 29);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(173, 23);
+            this.txtCity.TabIndex = 7;
+            // 
+            // lbCity
+            // 
+            this.lbCity.AutoSize = true;
+            this.lbCity.Location = new System.Drawing.Point(381, 32);
+            this.lbCity.Name = "lbCity";
+            this.lbCity.Size = new System.Drawing.Size(28, 15);
+            this.lbCity.TabIndex = 6;
+            this.lbCity.Text = "City";
+            // 
+            // txtCountry
+            // 
+            this.txtCountry.Location = new System.Drawing.Point(454, 73);
+            this.txtCountry.Name = "txtCountry";
+            this.txtCountry.Size = new System.Drawing.Size(173, 23);
+            this.txtCountry.TabIndex = 9;
+            // 
+            // lbCountry
+            // 
+            this.lbCountry.AutoSize = true;
+            this.lbCountry.Location = new System.Drawing.Point(381, 76);
+            this.lbCountry.Name = "lbCountry";
+            this.lbCountry.Size = new System.Drawing.Size(50, 15);
+            this.lbCountry.TabIndex = 8;
+            this.lbCountry.Text = "Country";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(454, 117);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(173, 23);
+            this.txtPassword.TabIndex = 11;
+            // 
+            // lbPassword
+            // 
+            this.lbPassword.AutoSize = true;
+            this.lbPassword.Location = new System.Drawing.Point(381, 120);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Size = new System.Drawing.Size(57, 15);
+            this.lbPassword.TabIndex = 10;
+            this.lbPassword.Text = "Password";
+            // 
+            // dgvMemberList
+            // 
+            this.dgvMemberList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMemberList.Location = new System.Drawing.Point(31, 228);
+            this.dgvMemberList.Name = "dgvMemberList";
+            this.dgvMemberList.RowTemplate.Height = 25;
+            this.dgvMemberList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvMemberList.Size = new System.Drawing.Size(596, 150);
+            this.dgvMemberList.TabIndex = 12;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(31, 181);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 13;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(293, 181);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(75, 23);
+            this.btnNew.TabIndex = 14;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(552, 181);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 15;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(293, 391);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 16;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // frmMembers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmMembers";
+            this.ClientSize = new System.Drawing.Size(660, 426);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.dgvMemberList);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lbPassword);
+            this.Controls.Add(this.txtCountry);
+            this.Controls.Add(this.lbCountry);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.lbCity);
+            this.Controls.Add(this.txtCompanyName);
+            this.Controls.Add(this.lbCompanyName);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lbEmail);
+            this.Controls.Add(this.txtMemberId);
+            this.Controls.Add(this.lbMemberId);
+            this.Name = "frmMembers";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Members Management";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMemberList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label lbMemberId;
+        private TextBox txtMemberId;
+        private TextBox txtEmail;
+        private Label lbEmail;
+        private TextBox txtCompanyName;
+        private Label lbCompanyName;
+        private TextBox txtCity;
+        private Label lbCity;
+        private TextBox txtCountry;
+        private Label lbCountry;
+        private TextBox txtPassword;
+        private Label lbPassword;
+        private DataGridView dgvMemberList;
+        private Button btnLoad;
+        private Button btnNew;
+        private Button btnDelete;
+        private Button btnClose;
     }
 }
