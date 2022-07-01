deb49429a5effaa54a280a2c1ab59ea0cdf365d0
GuuNguyem <guuhighschool@gmail.com>
2022-07-01 21:32:57 +0700

    CRUD done

--- DataAccess/ProductDAO.cs
+++ DataAccess/ProductDAO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataAccess.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,97 @@ using System.Threading.Tasks;
 
 namespace DataAccess
 {
-    internal class ProductDAO
+    public class ProductDAO
     {
+        //singleton pattern
+        private static ProductDAO? instance { get; set; }
+        private static readonly object instanceLock = new object();
+        private ProductDAO() { }
+        public static ProductDAO Instance
+        {
+            get
+            {
+                lock (instanceLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new ProductDAO();
+                    }
+                    return instance;
+                }
+            }
+        }
+        public IEnumerable<Product> GetAllProducts()
+        {
+            var products = new List<Product>();
+            try
+            {
+                using FStoreASM2Context context = new();
+                context.Products.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return products;
+        }
+        public Product GetProductById(Product product)
+        {
+            Product? pro;
+            try
+            {
+                using FStoreASM2Context context = new();
+                pro = context.Products.SingleOrDefault(p => p.ProductId == product.ProductId);
+                if(pro == null)
+                {
+                    throw new Exception("This product does not exist!");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return pro;
+        }
+        public void InsertProduct(Product product)
+        {
+            try
+            {
+                using FStoreASM2Context context = new();
+                context.Products.Add(product);
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public void UpdateProduct(Product product)
+        {
+            try
+            {
+                using FStoreASM2Context context = new();
+                context.Entry<Product>(product).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public void DeleteProduct(Product product)
+        {
+            try
+            {
+                using FStoreASM2Context context = new();
+                var pro = GetProductById(product);
+                context.Products.Remove(pro);
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
     }
 }
