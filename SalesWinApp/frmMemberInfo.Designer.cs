b7836d21ab9d0f3a095a872111acca12d45642b9
thanhnhan1402 <nhanttse151229@fpt.edu.vn>
2022-07-01 16:09:26 +0700

    Design MemberInfo form

---
+++ SalesWinApp/frmMemberInfo.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace SalesWinApp
+{
+    partial class frmMemberInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbMemberId = new System.Windows.Forms.Label();
+            this.txtMemberId = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lbEmail = new System.Windows.Forms.Label();
+            this.txtCompanyName = new System.Windows.Forms.TextBox();
+            this.lbCompanyName = new System.Windows.Forms.Label();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.lbCity = new System.Windows.Forms.Label();
+            this.txtCountry = new System.Windows.Forms.TextBox();
+            this.lbCountry = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lbPassword = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbMemberId
+            // 
+            this.lbMemberId.AutoSize = true;
+            this.lbMemberId.Location = new System.Drawing.Point(26, 23);
+            this.lbMemberId.Name = "lbMemberId";
+            this.lbMemberId.Size = new System.Drawing.Size(66, 15);
+            this.lbMemberId.TabIndex = 0;
+            this.lbMemberId.Text = "Member ID";
+            // 
+            // txtMemberId
+            // 
+            this.txtMemberId.Location = new System.Drawing.Point(126, 20);
+            this.txtMemberId.Name = "txtMemberId";
+            this.txtMemberId.Size = new System.Drawing.Size(166, 23);
+            this.txtMemberId.TabIndex = 0;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(126, 63);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(166, 23);
+            this.txtEmail.TabIndex = 1;
+            // 
+            // lbEmail
+            // 
+            this.lbEmail.AutoSize = true;
+            this.lbEmail.Location = new System.Drawing.Point(26, 63);
+            this.lbEmail.Name = "lbEmail";
+            this.lbEmail.Size = new System.Drawing.Size(36, 15);
+            this.lbEmail.TabIndex = 2;
+            this.lbEmail.Text = "Email";
+            // 
+            // txtCompanyName
+            // 
+            this.txtCompanyName.Location = new System.Drawing.Point(126, 107);
+            this.txtCompanyName.Name = "txtCompanyName";
+            this.txtCompanyName.Size = new System.Drawing.Size(166, 23);
+            this.txtCompanyName.TabIndex = 2;
+            // 
+            // lbCompanyName
+            // 
+            this.lbCompanyName.AutoSize = true;
+            this.lbCompanyName.Location = new System.Drawing.Point(26, 107);
+            this.lbCompanyName.Name = "lbCompanyName";
+            this.lbCompanyName.Size = new System.Drawing.Size(94, 15);
+            this.lbCompanyName.TabIndex = 4;
+            this.lbCompanyName.Text = "Company Name";
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(126, 148);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(166, 23);
+            this.txtCity.TabIndex = 3;
+            // 
+            // lbCity
+            // 
+            this.lbCity.AutoSize = true;
+            this.lbCity.Location = new System.Drawing.Point(26, 151);
+            this.lbCity.Name = "lbCity";
+            this.lbCity.Size = new System.Drawing.Size(28, 15);
+            this.lbCity.TabIndex = 6;
+            this.lbCity.Text = "City";
+            // 
+            // txtCountry
+            // 
+            this.txtCountry.Location = new System.Drawing.Point(126, 190);
+            this.txtCountry.Name = "txtCountry";
+            this.txtCountry.Size = new System.Drawing.Size(166, 23);
+            this.txtCountry.TabIndex = 4;
+            // 
+            // lbCountry
+            // 
+            this.lbCountry.AutoSize = true;
+            this.lbCountry.Location = new System.Drawing.Point(26, 198);
+            this.lbCountry.Name = "lbCountry";
+            this.lbCountry.Size = new System.Drawing.Size(50, 15);
+            this.lbCountry.TabIndex = 8;
+            this.lbCountry.Text = "Country";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(126, 229);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(166, 23);
+            this.txtPassword.TabIndex = 5;
+            // 
+            // lbPassword
+            // 
+            this.lbPassword.AutoSize = true;
+            this.lbPassword.Location = new System.Drawing.Point(26, 237);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Size = new System.Drawing.Size(57, 15);
+            this.lbPassword.TabIndex = 10;
+            this.lbPassword.Text = "Password";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(126, 275);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(217, 275);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 7;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // frmMemberInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 308);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lbPassword);
+            this.Controls.Add(this.txtCountry);
+            this.Controls.Add(this.lbCountry);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.lbCity);
+            this.Controls.Add(this.txtCompanyName);
+            this.Controls.Add(this.lbCompanyName);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lbEmail);
+            this.Controls.Add(this.txtMemberId);
+            this.Controls.Add(this.lbMemberId);
+            this.Name = "frmMemberInfo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Member Info";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lbMemberId;
+        private TextBox txtMemberId;
+        private TextBox txtEmail;
+        private Label lbEmail;
+        private TextBox txtCompanyName;
+        private Label lbCompanyName;
+        private TextBox txtCity;
+        private Label lbCity;
+        private TextBox txtCountry;
+        private Label lbCountry;
+        private TextBox txtPassword;
+        private Label lbPassword;
+        private Button btnSave;
+        private Button btnCancel;
+    }
+}
