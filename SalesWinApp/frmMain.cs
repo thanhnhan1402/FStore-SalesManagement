92dddd0d3ac49d09c6953b125b1ede67c48e30bc
thanhnhan1402 <nhanttse151229@fpt.edu.vn>
2022-07-01 15:01:07 +0700

    Refactor Main form

--- SalesWinApp/frmMain.cs
+++ SalesWinApp/frmMain.cs
@@ -12,9 +12,54 @@ namespace SalesWinApp
 {
     public partial class frmMain : Form
     {
+        frmMembers frmMembers;
+        frmOrders frmOrders;
+        frmProducts frmProducts;
         public frmMain()
         {
             InitializeComponent();
+        }
+
+        private void membersToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (frmMembers == null)
+            {
+                frmMembers = new frmMembers();
+                frmMembers.MdiParent = this;
+                frmMembers.Show();
+            } 
+            else
+            {
+                frmMembers.Activate();
+            }
+        }
+
+        private void productsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (frmProducts == null)
+            {
+                frmProducts = new frmProducts();
+                frmProducts.MdiParent = this;
+                frmProducts.Show();
+            }
+            else
+            {
+                frmProducts.Activate();
+            }
+        }
+
+        private void ordersToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (frmOrders == null)
+            {
+                frmOrders = new frmOrders();
+                frmOrders.MdiParent = this;
+                frmOrders.Show();
+            } 
+            else
+            {
+                frmOrders.Activate();
+            }
         }
     }
 }
