ac5d7eedb7b92fa8049cc832c30bd3674c3f2385
GuuNguyem <guuhighschool@gmail.com>
2022-07-10 22:34:26 +0700

    fix form history

--- SalesWinApp/frmHistory.Designer.cs
+++ SalesWinApp/frmHistory.Designer.cs
@@ -28,43 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.lbHistory = new System.Windows.Forms.Label();
             this.dgvHistory = new System.Windows.Forms.DataGridView();
+            this.orderBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.orderIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.memberIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.orderDateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.requiredDateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.shippedDateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.freightDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dgvHistory)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orderBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // lbHistory
             // 
             this.lbHistory.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.lbHistory.Location = new System.Drawing.Point(170, 9);
+            this.lbHistory.Location = new System.Drawing.Point(194, 12);
             this.lbHistory.Name = "lbHistory";
-            this.lbHistory.Size = new System.Drawing.Size(355, 63);
+            this.lbHistory.Size = new System.Drawing.Size(406, 84);
             this.lbHistory.TabIndex = 0;
             this.lbHistory.Text = "Member\'s Order History";
             // 
             // dgvHistory
             // 
+            this.dgvHistory.AutoGenerateColumns = false;
             this.dgvHistory.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvHistory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvHistory.Location = new System.Drawing.Point(22, 75);
+            this.dgvHistory.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.orderIdDataGridViewTextBoxColumn,
+            this.memberIdDataGridViewTextBoxColumn,
+            this.orderDateDataGridViewTextBoxColumn,
+            this.requiredDateDataGridViewTextBoxColumn,
+            this.shippedDateDataGridViewTextBoxColumn,
+            this.freightDataGridViewTextBoxColumn});
+            this.dgvHistory.DataSource = this.orderBindingSource;
+            this.dgvHistory.Location = new System.Drawing.Point(25, 100);
+            this.dgvHistory.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dgvHistory.Name = "dgvHistory";
+            this.dgvHistory.RowHeadersWidth = 51;
             this.dgvHistory.RowTemplate.Height = 25;
             this.dgvHistory.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvHistory.Size = new System.Drawing.Size(673, 150);
+            this.dgvHistory.Size = new System.Drawing.Size(769, 200);
             this.dgvHistory.TabIndex = 1;
+            // 
+            // orderBindingSource
+            // 
+            this.orderBindingSource.DataSource = typeof(DataAccess.Models.Order);
+            // 
+            // orderIdDataGridViewTextBoxColumn
+            // 
+            this.orderIdDataGridViewTextBoxColumn.DataPropertyName = "OrderId";
+            this.orderIdDataGridViewTextBoxColumn.HeaderText = "OrderId";
+            this.orderIdDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.orderIdDataGridViewTextBoxColumn.Name = "orderIdDataGridViewTextBoxColumn";
+            // 
+            // memberIdDataGridViewTextBoxColumn
+            // 
+            this.memberIdDataGridViewTextBoxColumn.DataPropertyName = "MemberId";
+            this.memberIdDataGridViewTextBoxColumn.HeaderText = "MemberId";
+            this.memberIdDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.memberIdDataGridViewTextBoxColumn.Name = "memberIdDataGridViewTextBoxColumn";
+            // 
+            // orderDateDataGridViewTextBoxColumn
+            // 
+            this.orderDateDataGridViewTextBoxColumn.DataPropertyName = "OrderDate";
+            this.orderDateDataGridViewTextBoxColumn.HeaderText = "OrderDate";
+            this.orderDateDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.orderDateDataGridViewTextBoxColumn.Name = "orderDateDataGridViewTextBoxColumn";
+            // 
+            // requiredDateDataGridViewTextBoxColumn
+            // 
+            this.requiredDateDataGridViewTextBoxColumn.DataPropertyName = "RequiredDate";
+            this.requiredDateDataGridViewTextBoxColumn.HeaderText = "RequiredDate";
+            this.requiredDateDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.requiredDateDataGridViewTextBoxColumn.Name = "requiredDateDataGridViewTextBoxColumn";
+            // 
+            // shippedDateDataGridViewTextBoxColumn
+            // 
+            this.shippedDateDataGridViewTextBoxColumn.DataPropertyName = "ShippedDate";
+            this.shippedDateDataGridViewTextBoxColumn.HeaderText = "ShippedDate";
+            this.shippedDateDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.shippedDateDataGridViewTextBoxColumn.Name = "shippedDateDataGridViewTextBoxColumn";
+            // 
+            // freightDataGridViewTextBoxColumn
+            // 
+            this.freightDataGridViewTextBoxColumn.DataPropertyName = "Freight";
+            this.freightDataGridViewTextBoxColumn.HeaderText = "Freight";
+            this.freightDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.freightDataGridViewTextBoxColumn.Name = "freightDataGridViewTextBoxColumn";
             // 
             // frmHistory
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(716, 270);
+            this.ClientSize = new System.Drawing.Size(818, 360);
             this.Controls.Add(this.dgvHistory);
             this.Controls.Add(this.lbHistory);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "frmHistory";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmHistory";
             this.Load += new System.EventHandler(this.frmHistory_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvHistory)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orderBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -73,5 +141,12 @@
 
         private Label lbHistory;
         private DataGridView dgvHistory;
+        private DataGridViewTextBoxColumn orderIdDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn memberIdDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn orderDateDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn requiredDateDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn shippedDateDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn freightDataGridViewTextBoxColumn;
+        private BindingSource orderBindingSource;
     }
 }
