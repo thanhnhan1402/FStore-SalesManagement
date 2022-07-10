4269b35b8bc38fb464925dea6dc8231d54b4fa5f
thanhnhan1402 <nhanttse151229@fpt.edu.vn>
2022-07-10 13:40:48 +0700

    Design and code History form

---
+++ SalesWinApp/frmHistory.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace SalesWinApp
+{
+    partial class frmHistory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbHistory = new System.Windows.Forms.Label();
+            this.dgvHistory = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHistory)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbHistory
+            // 
+            this.lbHistory.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbHistory.Location = new System.Drawing.Point(170, 9);
+            this.lbHistory.Name = "lbHistory";
+            this.lbHistory.Size = new System.Drawing.Size(355, 63);
+            this.lbHistory.TabIndex = 0;
+            this.lbHistory.Text = "Member\'s Order History";
+            // 
+            // dgvHistory
+            // 
+            this.dgvHistory.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvHistory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvHistory.Location = new System.Drawing.Point(22, 75);
+            this.dgvHistory.Name = "dgvHistory";
+            this.dgvHistory.RowTemplate.Height = 25;
+            this.dgvHistory.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvHistory.Size = new System.Drawing.Size(673, 150);
+            this.dgvHistory.TabIndex = 1;
+            // 
+            // frmHistory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 270);
+            this.Controls.Add(this.dgvHistory);
+            this.Controls.Add(this.lbHistory);
+            this.Name = "frmHistory";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmHistory";
+            this.Load += new System.EventHandler(this.frmHistory_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHistory)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Label lbHistory;
+        private DataGridView dgvHistory;
+    }
+}
---
+++ SalesWinApp/frmHistory.cs
@@ -0,0 +1,29 @@
+﻿using DataAccess.Repository;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalesWinApp
+{
+    public partial class frmHistory : Form
+    {
+        IOrderRepository orderRepository = new OrderRepository();
+        public int MemberId { get; set; }
+        public frmHistory()
+        {
+            InitializeComponent();
+        }
+        private void frmHistory_Load(object sender, EventArgs e)
+        {
+            dgvHistory.DataSource = orderRepository.GetHistory(MemberId);
+            dgvHistory.Columns["Member"].Visible = false;
+        }
+    }
+
+}
